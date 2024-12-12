cd4f5a00466b49b478d8aa0ef51697abb8a2e59d
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-12-13 00:47:45 +0530

    updated the bean files

--- src/main/java/com/bean/UserProfile.java
+++ src/main/java/com/bean/UserProfile.java
@@ -13,6 +13,60 @@ public class UserProfile {
 	Gender gender;
 	Date created_at;
 	Date updated_at;
+	public int getProfile_id() {
+		return profile_id;
+	}
+	public void setProfile_id(int profile_id) {
+		this.profile_id = profile_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+	public String getFull_name() {
+		return full_name;
+	}
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public Date getDate_of_birth() {
+		return date_of_birth;
+	}
+	public void setDate_of_birth(Date date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public Date getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
 	
 	 
 }
