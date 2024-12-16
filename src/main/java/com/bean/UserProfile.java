6d2121fb89b1407bd9b89902610d31ef179f8d9d
Ankushk-01 <ankushkumar.me24@gmail.com>
2024-12-16 23:21:04 +0530

    updated the profile file

--- src/main/java/com/bean/UserProfile.java
+++ src/main/java/com/bean/UserProfile.java
@@ -14,6 +14,29 @@ public class UserProfile {
 	Gender gender;
 	Timestamp created_at;
 	Timestamp updated_at;
+	String email;
+	String password;
+	String cpassword;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	
 	public int getProfile_id() {
 		return profile_id;
 	}
